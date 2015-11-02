a570eba6890a680f0a5492d60e28febbf61ee7dd
totzYuta <yuta.totz@gmail.com>
2015-11-03 01:00:13 +0900

    Comment#create by curl

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,4 +2,15 @@ class Api::V1::CommentsController < ApplicationController
   def index
     @data = Comment.all
   end
+
+  def create
+    @comment = Comment.create(comment_prams)
+    render :show, status: :created
+  end
+
+  private
+
+  def comment_prams
+    params.permit(:author, :text)
+  end
 end
